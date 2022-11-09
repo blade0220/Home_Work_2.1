e4269346bf4a6ceca682fd238e578b7a5b80f475
blade0220 <blade0220@yandex.ru>
2022-11-09 18:43:28 +0300

    Моё первое ДЗ по второму курсу. Прошу прощения за задержку

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    public String brand;
+    public String model;
+    public float engineVolume;
+    public String color;
+    public int year;
+    public String country;
+
+    public Car(String brand, String model, float engineVolume, String color, int year, String country) {
+        this.brand = validOrDefault(brand, "Default");
+        this.model = validOrDefault(model, "default");
+        this.engineVolume = engineVolume > 0.0f ? engineVolume : 1.5f;
+        this.color = validOrDefault(color, "White");
+        this.year = year >= 0 ? year : 2000;
+        this.country = validOrDefault(country, "default");
+    }
+
+    public static String validOrDefault(String value, String defaultValue){
+        return value == null || value.isBlank() ? defaultValue : value;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Автомобиль {" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args){
+        Human maxim = new Human("Максим", "Минск",1988,
+                "бренд-менеджер");
+
+        System.out.println(maxim);
+
+        Human anya = new Human("Аня","Москва",1993,
+                "методист образовательных программ");
+
+        System.out.println(anya);
+
+        Human katya = new Human("Катя","Калининград",1992,
+                "продакт-менеджер");
+
+        System.out.println(katya);
+
+        Human artyom = new Human("Артём","Москва",1995,
+                "директор по развитию бизнеса");
+
+        System.out.println(artyom);
         System.out.println();
+
+        Car lada = new Car("Lada","Granta",1.7f,"желтый",2015,"Россия");
+
+        System.out.println(lada);
+
+        Car audi = new Car("Audi","A8 50 L TDI quattro",3.0f,
+                "Черный",2020,"Германия");
+
+        System.out.println(audi);
+
+        Car bmw = new Car("BMW","Z8",3.0f,
+                "Черный",2021,"Германия");
+
+        System.out.println(bmw);
+
+        Car kia = new Car("KIA","Sportage 4-го поколения",2.4f,
+                "Красный",2018,"Южная Корея");
+
+        System.out.println(kia);
+
+        Car hyundai = new Car("Hyundai","Avante",1.6f,
+                "Оранжевый",2016,"Южная Корея");
+
+        System.out.println(hyundai);
+
     }
 }
